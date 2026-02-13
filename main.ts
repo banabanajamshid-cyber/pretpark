cc10f1cd029000068811353cc9ca954694b3edd9
banabanajamshid-cyber <banabanajamshid@gmail.com>
2026-02-13 09:45:27 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.forever(function () {
+    music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+})
+basic.forever(function () {
+    basic.showString("AQUALANTIS")
+})
